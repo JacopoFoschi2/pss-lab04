bb599accce96b017b01ee1c2a11caef226b6539e
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2025-11-04 10:48:23 +0100

    added print of treeset

--- 01-use-set/src/main/java/it/unibo/collections/sets/UseSet.java
+++ 01-use-set/src/main/java/it/unibo/collections/sets/UseSet.java
@@ -35,6 +35,7 @@ public final class UseSet {
         /*
          * 3) Prints its content
          */
+        System.out.println(set);
         /*
          * 4) Removes all those strings whose represented number is divisible by three.
          * Note: the method removeIf(Predicate) is not allowed.
